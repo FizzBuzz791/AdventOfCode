22f3d5aef5aa28146506b3de709372d003dfcb2d
Trent Jones <trentwilliamjones@gmail.com>
2021-12-03 15:34:47 +0800

    2021 - Day 4 - Base

---
+++ AdventOfCode/Year2021/Day4/Solution.cs
@@ -0,0 +1,22 @@
+using System;
+using NAoCHelper;
+
+namespace AdventOfCode.Year2021.Day4
+{
+    public class Solution : BaseSolution<string[]>, ISolvable
+    {
+        public Solution(IPuzzle puzzle) : base(puzzle, StringArraySelector)
+        {
+        }
+
+        public string SolvePart1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SolvePart2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
