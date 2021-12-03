e46fa4e9de2b6105db8e6683c80084d04cd5e2d8
Trent Jones <trentwilliamjones@gmail.com>
2021-12-03 14:00:20 +0800

    2021 - Day 2 - Part 1

--- AdventOfCode/BaseSolution.cs
+++ AdventOfCode/BaseSolution.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Linq;
 using NAoCHelper;
 
 namespace AdventOfCode
 {
     public abstract class BaseSolution<T>
     {
+        public static readonly Func<string, int[]> IntArraySelector = x => x.Trim('\n').Split("\n").Select(int.Parse).ToArray();
+        public static readonly Func<string, string[]> StringArraySelector = x => x.Trim('\n').Split("\n");
+
         protected T Input { get; }
 
         protected BaseSolution(IPuzzle puzzle, Func<string, T> inputSelector) =>
