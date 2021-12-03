46a425fe71978ae3e4267a74d3e12e8ad3687cbf
Trent Jones <trentwilliamjones@gmail.com>
2021-12-03 15:29:01 +0800

    2021 - Day 3 - Part 2

--- AdventOfCode/Year2021/Day3/Solution.cs
+++ AdventOfCode/Year2021/Day3/Solution.cs
@@ -31,7 +31,48 @@ namespace AdventOfCode.Year2021.Day3
 
         public string SolvePart2()
         {
-            return $"Part 2:";
+            var length = Input[0].Length;
+            var binaryOxygenRating = "";
+            var binaryCO2Rating = "";
+            var validOxygenResults = Input;
+            var validCO2Results = Input;
+
+            var counter = 0;
+            while (validOxygenResults.Length > 1)
+            {
+                var bitsAtIndex = validOxygenResults.Select(x => x[counter]);
+                var countOfOnes = bitsAtIndex.Count(x => x == '1');
+                var countOfZeroes = bitsAtIndex.Count(x => x == '0');
+
+                validOxygenResults = countOfOnes >= countOfZeroes
+                    ? validOxygenResults.Where(x => x[counter] == '1').ToArray()
+                    : validOxygenResults.Where(x => x[counter] == '0').ToArray();
+
+                counter++;
+            }
+            binaryOxygenRating = validOxygenResults[0];
+
+            // Reset counter
+            counter = 0;
+
+            while (validCO2Results.Length > 1)
+            {
+                var bitsAtIndex = validCO2Results.Select(x => x[counter]);
+                var countOfOnes = bitsAtIndex.Count(x => x == '1');
+                var countOfZeroes = bitsAtIndex.Count(x => x == '0');
+
+                validCO2Results = countOfZeroes <= countOfOnes
+                    ? validCO2Results.Where(x => x[counter] == '0').ToArray()
+                    : validCO2Results.Where(x => x[counter] == '1').ToArray();
+
+                counter++;
+            }
+            binaryCO2Rating = validCO2Results[0];
+
+            var oxygenRate = Convert.ToInt32(binaryOxygenRating, 2);
+            var CO2Rate = Convert.ToInt32(binaryCO2Rating, 2);
+
+            return $"Part 2: {oxygenRate * CO2Rate}";
         }
     }
 }
