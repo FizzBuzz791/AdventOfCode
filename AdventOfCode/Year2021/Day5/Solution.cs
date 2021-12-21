b7dc150f256b8aed7c2a836fabe0e8e621903e7e
Trent Jones <trent.jones@purple.telstra.com>
2021-12-21 17:57:06 +0800

    2021 - Day 5 - Part 1

--- AdventOfCode/Year2021/Day5/Solution.cs
+++ AdventOfCode/Year2021/Day5/Solution.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 using NAoCHelper;
 
 namespace AdventOfCode.Year2021.Day5
@@ -10,7 +13,43 @@ namespace AdventOfCode.Year2021.Day5
 
         public string SolvePart1()
         {
-            return $"Part 1: Not yet implemented.";
+            var dict = new Dictionary<string, int>();
+
+            foreach (var line in Input)
+            {
+                var parts = line.Split(" -> ");
+                var startParts = parts[0].Split(',').Select(int.Parse).ToArray();
+                var endParts = parts[1].Split(',').Select(int.Parse).ToArray();
+                var start = new Point(startParts[0], startParts[1]);
+                var end = new Point(endParts[0], endParts[1]);
+
+                // horizontal and vertical lines
+                if (start.X == end.X || start.Y == end.Y)
+                {
+                    var xIncreasing = start.X < end.X;
+                    for (int i = xIncreasing ? start.X : end.X; i <= (xIncreasing ? end.X : start.X); i++)
+                    {
+                        var yIncreasing = start.Y < end.Y;
+                        for (int j = yIncreasing ? start.Y : end.Y; j <= (yIncreasing ? end.Y : start.Y); j++)
+                        {
+                            var point = $"{i},{j}";
+                            if (dict.ContainsKey(point))
+                            {
+                                dict[point]++;
+                            }
+                            else
+                            {
+                                dict.Add(point, 1);
+                            }
+                        }
+                    }
+                }
+            }
+
+            var result = dict.Count(d => d.Value > 1);
+
+            // 1322 is too low
+            return $"Part 1: {result}.";
         }
 
         public string SolvePart2()
